d1f705def57a288279060871f7f4f7c1001f3a3b
Francois Rakoto <50391876+HDRHawking@users.noreply.github.com>
2024-02-04 07:18:18 +0300

    add model reservation and service

---
+++ models/reservation.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  id_: {
+    type : String
+  },
+  idserv: {
+    type : String
+  },
+  userid: {
+    type : String
+  },
+  dateCreatReservation: {
+    type : Date,
+    default: Date.now
+  },
+  dateReservation: {
+    type : String
+  },
+  dateFinReservation: {
+    type : String
+  },
+})
+
+var reservation = new mongoose.model('reservation', schema);
+
+module.exports = reservation;
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,5 @@ var router = express.Router();
 const userController = require('../controllers/userController');
 router.post('/signin', userController.signin);
 router.post('/signup', userController.signup);
+router.post('/reservation', userController.getAppointment);
 module.exports = router;
